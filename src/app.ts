6a30ed7a59ca58b5632824eec4a3adfe6592b8d8
Moppler <findmoppler@gmail.com>
2021-07-22 15:23:49 +0100

    Application Config Fixes #20

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import ModelFactory from './modelFactory';
+import DaoFactory from './daoFactory';
 import Game from './game';
-
 import telnet from './networking/telnetServer';
 import commandFactory from './commandFactory';
-import DaoFactory from './daoFactory';
+import environment from '../config/environment';
 
 const modelFactory = new ModelFactory();
 const daoFactory = new DaoFactory();
@@ -13,6 +13,6 @@ game.initialise();
 
 // Telnet Server
 const telnetServer = telnet(game);
-telnetServer.listen(3200, () =>
-  console.log('Listening to telnet connections on 3100')
+telnetServer.listen(environment.telnetPort, () =>
+  console.log(`Listening to telnet connections on ${environment.telnetPort}`)
 );
